0fe6df754a9127ce191d85f84ee853f34e663545
Alexander Bareyko <militaryboy@yandex.ru>
2025-06-01 21:56:07 +0200

    fix: fix flake8

--- src/constants.py
+++ src/constants.py
@@ -18,5 +18,6 @@ VALIDATION_DATASET_FILE_NAME = PROCESSED_DATA_DIR / "validation_preprocessed.csv
 
 TARGET_COLUMN = "is_fraud"
 
-MODEL_NAME = os.environ.get("MODEL_NAME", "outputs/models/model.pkl")  # defaulted to model it is the name of the model after deployment
+# defaulted to model it is the name of the model after deployment
+MODEL_NAME = os.environ.get("MODEL_NAME", "outputs/models/model.pkl")
 MLFLOW_URI = os.environ.get("MLFLOW_URI", "http://127.0.0.1:5000/")
